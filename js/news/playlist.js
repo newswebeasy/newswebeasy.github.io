b79a1ed52fe60e34dff6cca03f628a3660b8d0f1
newswebeasy <newswebeasy@users.noreply.github.com>
2018-11-18 15:19:05 +0800

    playlist.css

---
+++ js/news/playlist.js
@@ -0,0 +1,45 @@
+var playing = null;
+var playlist = document.getElementsByTagName('li');
+
+var player = document.getElementById('player');
+player.load();
+
+var repeat_one = false;
+player.onended = function () {
+  console.log('end');
+  if (repeat_one) {
+    player.play();
+  } else {
+    var next = playing.nextElementSibling || playlist[0];
+    console.log('next');
+    console.log(next);
+    play(next);
+  }
+}
+player.onplay = function (e) {
+  playing.classList.add("playing");
+}
+player.onpause = function (e) {
+  playing.classList.remove("playing");
+}
+
+function play(el) {
+  if (el != playing) {
+    var last = playing;
+    playing = el;
+    player.src = playing.dataset.voiceUrl;
+    player.load();
+
+    if (last) {
+      last.classList.remove("playing");
+    }
+  }
+
+  if (player.paused) {
+    player.play();
+  }
+
+  return false;
+}
+
+play(playlist[0]);
